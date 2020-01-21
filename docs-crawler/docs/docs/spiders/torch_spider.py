b3e76019a7c2862a9036dcdf6d55d5e1f87c0859
amrmkayid <amrkayid2027@gmail.com>
2020-01-21 17:25:19 +0200

    Change TorchSpider to extend CrawlSpider and use rules for crawling

--- docs-crawler/docs/docs/spiders/torch_spider.py
+++ docs-crawler/docs/docs/spiders/torch_spider.py
@@ -1,55 +1,34 @@
 import re
 import scrapy
 from docs.items import ApiItem
+from scrapy.spiders import Rule, CrawlSpider
+from scrapy.linkextractors import LinkExtractor
 from w3lib.html import remove_tags
 
 
-class TorchSpider(scrapy.Spider):
+class TorchSpider(CrawlSpider):
     name = "torch"
     version = "1.4.0"
+    allowed_domains = ['pytorch.org']
+    start_urls = [f'https://pytorch.org/docs/{version}/index.html']
     split_def = re.compile('^([\w\.]+)\(([\w\,\s=\*\.]*)\)')
 
-    def start_requests(self):
-        urls = [
-            f'https://pytorch.org/docs/{self.version}/torch.html',
-            f'https://pytorch.org/docs/{self.version}/nn.html',
-            f'https://pytorch.org/docs/{self.version}/nn.functional.html',
-            f'https://pytorch.org/docs/{self.version}/tensors.html',
-            f'https://pytorch.org/docs/{self.version}/tensor_attributes.html',
-            f'https://pytorch.org/docs/{self.version}/autograd.html',
-            f'https://pytorch.org/docs/{self.version}/cuda.html',
-            f'https://pytorch.org/docs/{self.version}/distributed.html',
-            f'https://pytorch.org/docs/{self.version}/distributions.html',
-            f'https://pytorch.org/docs/{self.version}/hub.html',
-            f'https://pytorch.org/docs/{self.version}/jit.html',
-            f'https://pytorch.org/docs/{self.version}/nn.init.html',
-            f'https://pytorch.org/docs/{self.version}/onnx.html',
-            f'https://pytorch.org/docs/{self.version}/optim.html',
-            f'https://pytorch.org/docs/{self.version}/quantization.html',
-            f'https://pytorch.org/docs/{self.version}/rpc.html',
-            f'https://pytorch.org/docs/{self.version}/random.html',
-            f'https://pytorch.org/docs/{self.version}/sparse.html',
-            f'https://pytorch.org/docs/{self.version}/storage.html',
-            f'https://pytorch.org/docs/{self.version}/bottleneck.html',
-            f'https://pytorch.org/docs/{self.version}/checkpoint.html',
-            f'https://pytorch.org/docs/{self.version}/cpp_extension.html',
-            f'https://pytorch.org/docs/{self.version}/data.html',
-            f'https://pytorch.org/docs/{self.version}/dlpack.html',
-            f'https://pytorch.org/docs/{self.version}/model_zoo.html',
-            f'https://pytorch.org/docs/{self.version}/tensorboard.html',
-            f'https://pytorch.org/docs/{self.version}/type_info.html',
-            f'https://pytorch.org/docs/{self.version}/named_tensor.html',
-            f'https://pytorch.org/docs/{self.version}/name_inference.html',
-            f'https://pytorch.org/docs/{self.version}/__config__.html',
-        ]
-        for url in urls:
-            yield scrapy.Request(url=url, callback=self.parse)
-
-    def parse(self, response):
+    rules = (
+        Rule(LinkExtractor(
+                allow=(re.compile('.+\.html')),
+                restrict_css='.toctree-l1'), 
+            callback='parse_api',),
+    )
+    
+    def parse_api(self, response):
+        self.logger.info(f'Scraping {response.url}')
         fdef = response.css('dl.function > dt')
         defs = []
         for selector in fdef:
-            text = remove_tags(selector.get()).replace('\n', '')
+            text = (remove_tags(selector.get())
+                    .replace('\n', '')
+                    .replace(' ', '')
+                    .replace('[source]', ''))
             defs.append(text)
 
         for text in defs:
