614e76c34a32de4133ecb98409b26f8f524b6b75
Mike Nolan <me@michael-nolan.com>
2020-03-02 13:56:24 +0000

    test: beef up test suite with auto-translate tests

--- js/test/fixtures/pytorch.js
+++ js/test/fixtures/pytorch.js
@@ -4,56 +4,84 @@ import Command from '../../src/command';
 export const add = new Command(
   'torch.add(input,other,out=None',
   'add',
-  [tf.tensor([1]), tf.tensor([1])],
+  [
+    tf.tensor([
+      [1, 0],
+      [0, 1]
+    ]),
+    tf.tensor([
+      [0, 1],
+      [1, 0]
+    ])
+  ],
   [['out', 'None']]
 );
 
 export const matmul = new Command(
   'torch.matmul(input, other)',
   'matmul',
-  [tf.tensor([1]), tf.tensor([1])],
+  [
+    tf.tensor([
+      [1, 0],
+      [0, 1]
+    ]),
+    tf.tensor([
+      [0, 1],
+      [1, 0]
+    ])
+  ],
   [['out', 'None']]
 );
 
 export const mean = new Command(
   'torch.mean(input,dim,out=None)',
   'mean',
-  [tf.tensor([1]), 1],
+  [tf.tensor([0.2294, -0.5481, 1.3288])],
   []
 );
 
 export const mul = new Command(
   'torch.mul(input, other)',
-  'mean',
-  [tf.tensor([1]), tf.tensor([1])],
+  'mul',
+  [tf.tensor([10, 20, 30]), 100],
   [['out', 'None']]
 );
 
 export const div = new Command(
   'torch.div(input, other)',
   'div',
-  [tf.tensor([1]), tf.tensor([1])],
+  [tf.tensor([10, 20, 30, 40]), 0.5],
   [['out', 'None']]
 );
 
 export const eq = new Command(
   'torch.eq(input, other)',
   'eq',
-  [tf.tensor([1]), tf.tensor([1])],
+  [
+    tf.tensor([
+      [1, 2],
+      [3, 4]
+    ]),
+    tf.tensor([
+      [1, 1],
+      [4, 4]
+    ])
+  ],
+
   [['out', 'None']]
 );
 
 export const sum = new Command(
   'torch.sum(input, dim)',
   'sum',
-  [tf.tensor([1]), 1],
+  [tf.tensor([1, 2, 3])],
   [['out', 'None']]
 );
 
 export const argmax = new Command(
   'torch.argmax(input, dim)',
   'argmax',
-  [tf.tensor([1]), 1],
+  [tf.tensor([0, 10, 2])],
   [['out', 'None']]
 );
 
--- js/test/threepio.test.js
+++ js/test/threepio.test.js
@@ -1,23 +1,94 @@
+import * as tf from '@tensorflow/tfjs';
 import Threepio from '../src/threepio';
-import { add } from './fixtures/pytorch';
+import {
+  add,
+  matmul,
+  mean,
+  mul,
+  div,
+  eq,
+  sum,
+  argmax
+} from './fixtures/pytorch';
 const { test, describe, expect } = global; // import jest from global
 
 describe('Threepio', () => {
-  const brokenAdd = {
-    ...add,
-    kwargs: undefined
-  };
   const threepio = new Threepio({});
+
+  const checkAnswer = (result, answer) => {
+    const eq = tf
+      .equal(result, answer)
+      .all()
+      .dataSync();
+    expect(eq).toEqual(new Uint8Array([1]));
+  };
 
   test('returns itself', () => {
     expect(threepio).toBeInstanceOf(Threepio);
   });
 
-  test('translate basic function', () => {
-    threepio.translate('tfjs', add);
+  test('translates add', () => {
+    const execAdd = threepio.translate('tfjs', add);
+    const result = execAdd.executeRoutine();
+    const answer = tf.tensor([
+      [1, 1],
+      [1, 1]
+    ]);
+    checkAnswer(result, answer);
   });
 
-  test('ensure error', () => {
-    expect(() => threepio.translate(brokenAdd)).toThrow(Error);
+  test('translates matmul', () => {
+    const execadd = threepio.translate('tfjs', matmul);
+    const result = execadd.executeRoutine();
+    const answer = tf.tensor([
+      [0, 1],
+      [1, 0]
+    ]);
+    checkAnswer(result, answer);
+  });
+
+  test('translates mean', () => {
+    const execadd = threepio.translate('tfjs', mean);
+    const result = execadd.executeRoutine();
+    const answer = tf.tensor([0.3367]);
+    checkAnswer(result, answer);
+  });
+
+  test('translates mul', () => {
+    const execadd = threepio.translate('tfjs', mul);
+    const result = execadd.executeRoutine();
+    const answer = tf.tensor([1000, 2000, 3000]);
+    checkAnswer(result, answer);
+  });
+
+  test('translates div', () => {
+    const execadd = threepio.translate('tfjs', div);
+    const result = execadd.executeRoutine();
+    const answer = tf.tensor([20, 40, 60, 80]);
+    checkAnswer(result, answer);
+  });
+
+  test.skip('translates eq', () => {
+    const execadd = threepio.translate('tfjs', eq);
+    const result = execadd.executeRoutine();
+    const answer = tf.tensor([
+      [1, 0],
+      [0, 1]
+    ]);
+    checkAnswer(result, answer);
+  });
+
+  test('translates sum', () => {
+    const execadd = threepio.translate('tfjs', sum);
+    const result = execadd.executeRoutine();
+    const answer = tf.tensor([6]);
+    checkAnswer(result, answer);
+  });
+
+  test('translates argmax', () => {
+    const execadd = threepio.translate('tfjs', argmax);
+    const result = execadd.executeRoutine();
+    const answer = tf.tensor([1]);
+    checkAnswer(result, answer);
   });
 });
