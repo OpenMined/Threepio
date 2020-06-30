5b74e97e5e3ebddeefa176ab0ed3f7d1a48665e4
ramesht007 <shre611shukla@gmail.com>
2020-06-30 15:46:42 +0530

    # updated inline documentation

--- docs-crawler/docs/docs/spiders/tf_spider.py
+++ docs-crawler/docs/docs/spiders/tf_spider.py
@@ -1,3 +1,10 @@
+# _*_ coding: utf-8 -*-
+
+
+# Crawl modules in the TensorFlow docs to extract functions
+#
+# To understand how CrawlSpider works, See documentation in
+# https://docs.scrapy.org/en/latest/topics/spiders.html
 import re
 from docs.items import ApiItem
 from scrapy.spiders import Rule, CrawlSpider
@@ -6,21 +13,33 @@ from w3lib.html import remove_tags
 
 
 class TfSpider(CrawlSpider):
-    name = "tf"
-    version = "2.1"
-    allowed_domains = ['tensorflow.org']
+    name = "tf" # Name of the web crawler
+    version = "2.1" # Version of TesnorFlow documentation to crawl
+    allowed_domains = ['tensorflow.org'] # Crawls only links from TensorFlow
+
+
+    # Base URL for crawling
     start_urls = [
-        f'https://www.tensorflow.org/versions/r{version}/api_docs/python/tf'
-    ]
+        f'https://www.tensorflow.org/versions/r{version}/api_docs/python/tf']
+
+    
+    # Regex rules for compiling a string to a Regex object.
     split_def = re.compile(r'^([\w\.]+)\((.*)\)$')
 
+
+    # Rule(), guides the crawler starting at
+    # The reponse of the links direct to different TensorFlow modules
+    # and are passed to parse_api() for crawling.
     rules = (
         Rule(LinkExtractor(
-            allow=(re.compile(r'.+api_docs\/python\/tf')),
-            restrict_css='.devsite-nav-title'),
-            callback='parse_api',),
+            allow=(re.compile(r'.+api_docs\/python\/tf')), # allow the links under api_docs
+            restrict_css='.devsite-nav-title'), # Starts crawling from .devsite-nav-title.
+            callback='parse_api',), # calls parse_api() with response.
     )
 
+
+    # The parse_api() method is the callback method that parses the response 
+    # from each link extracted with the rule above.
     def parse_api(self, response):
         self.logger.info(f'Scraping {response.url}')
         item = ApiItem()
