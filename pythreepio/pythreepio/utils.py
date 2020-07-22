503d8ae0995a0a0a8b29dcfc67a96b673efdef2c
SOMIL JAIN <42209500+somiljain7@users.noreply.github.com>
2020-07-22 22:29:07 +0530

    #71

--- pythreepio/pythreepio/utils.py
+++ pythreepio/pythreepio/utils.py
@@ -1,15 +1,23 @@
-#importlib.resources is imported  to provide access to resources within packages
+# importlib.resources is imported  to provide access to resources within packages
+
 try:
     import importlib.resources as pkg_resources
-#for easier accessibility  importlib_resources can be refered to as pkg_resources
+    # for easier accessibility  importlib_resources can be refered to as pkg_resources
+
 except ImportError:
     import importlib_resources as pkg_resources
+    # except statement here represents importlib.resources in python3.6 and lower.   
+
 import json
 import static
-#static is imported to Serve static or templated content via WSGI 
+# static is imported to Serve static or templated content via WSGI 
 
-#this function is used to read data from "mapped_commands_full.json" file and returns an  dictionary  based on JSON file
-#'->' this represents data type of returning value of the function
+
+
 def get_mapped_commands() -> dict:
-    json_txt = pkg_resources.read_text(static, "mapped_commands_full.json")
-    return json.loads(json_txt)
+  """
+     this function is used to read data from "mapped_commands_full.json" file and returns an  dictionary  based on JSON file
+     '->' this represents data type of returning value of the function
+  """
+  json_txt = pkg_resources.read_text(static, "mapped_commands_full.json")
+  return json.loads(json_txt)
