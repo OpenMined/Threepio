e5f36e9695f77b82f363266ece82221788de7a0e
Justin Harris <justindharris@gmail.com>
2020-07-22 23:40:42 -0400

    py/translate: Map PyTorch tensor __rsub__ to sub for Tensorflow.js.

--- pythreepio/tests/test_torch_tfjs.py
+++ pythreepio/tests/test_torch_tfjs.py
@@ -1,9 +1,11 @@
 import pytest
+
 from pythreepio.threepio import Threepio
 from tests.fixtures.tfjs import abs as tfjs_abs
-from tests.fixtures.tfjs import to_float as tfjs_to_float
-from tests.fixtures.tfjs import rtruediv as tfjs_rtruediv
 from tests.fixtures.tfjs import reshape as tfjs_reshape
+from tests.fixtures.tfjs import rsub as tfjs_rsub
+from tests.fixtures.tfjs import rtruediv as tfjs_rtruediv
+from tests.fixtures.tfjs import to_float as tfjs_to_float
 
 
 @pytest.fixture
@@ -26,6 +28,16 @@ def test_translates_tfjs_to_float(tfjs_threepio):
             assert translation.args == tfjs_to_float["answers"][i].args
             assert translation.kwargs == tfjs_to_float["answers"][i].kwargs
             assert translation.attrs == tfjs_to_float["answers"][i].attrs
+
+
+def test_translates_tfjs_rsub(tfjs_threepio):
+  for input, answer in zip(tfjs_rsub["inputs"], tfjs_rsub["answers"]):
+    translations = tfjs_threepio.translate(input, lookup_command=True)
+    for translation in translations:
+      assert translation.function_name == answer.function_name
+      assert translation.args == answer.args
+      assert translation.kwargs == answer.kwargs
+      assert translation.attrs == answer.attrs
 
 
 def test_translates_tfjs_rtruediv(tfjs_threepio):
