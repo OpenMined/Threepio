8e3e8fee866ec2192200f374db825880edbd08e7
Darigov Research <30328618+darigovresearch@users.noreply.github.com>
2020-10-11 14:11:19 +0100

    feat: Adds additional documentation in sphinx format

--- pythreepio/pythreepio/utils.py
+++ pythreepio/pythreepio/utils.py
@@ -7,6 +7,10 @@ import static
 
 
 def get_mapped_commands() -> dict:
-    """Returns dictionary of mapped commands from mapped_commands_full.json"""
+    """Returns dictionary of mapped commands from mapped_commands_full.json
+        
+    :return: Dictionary of mapped commands
+    :rtype: dict
+    """
     json_txt = pkg_resources.read_text(static, "mapped_commands_full.json")
     return json.loads(json_txt)
