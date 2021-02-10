a36c208f9f63d23d4459a31a059224e0d50eca18
Praful932 <praful.mohanan@gmail.com>
2021-02-10 20:16:37 +0530

    Update package docs (#147)
    * Update package docs
    
    * Apply suggestions from code review
    
    Co-authored-by: Mike Nolan <me@michael-nolan.com>
    
    Co-authored-by: Mike Nolan <me@michael-nolan.com>

--- pythreepio/pythreepio/command.py
+++ pythreepio/pythreepio/command.py
@@ -3,12 +3,38 @@ from typing import Callable
 
 
 class Placeholder(object):
+    """
+    Helper Placeholder class to hold arguments of commands.
+
+    :param key: name of argument
+    :type key: str
+    :param value: value of argument
+    :type value: default(None)
+    """
+
     def __init__(self, key, value=None):
         self.key = key
         self.value = value
 
 
 class Command(object):
+    """
+    Command Class which holds commands and its details of a certain framework.
+
+    :param function_name: Command name in a particular framework
+    :type function_name: str
+    :param args: Arguments of the command For eg. Tensors to compute on
+    :type args: list
+    :param kwargs: Key value arguments of a command
+    :type kwargs: dict
+    :param attrs: Full API path of the command eg. tf.math.add
+    :type attrs: dict
+    :param placeholder_output: helper variable for 1->many translation
+    :type placeholder_output: str
+    :param exec_fn
+    :type Callable: store the command callable
+    """
+
     def __init__(
         self,
         function_name: str,
@@ -18,6 +44,7 @@ class Command(object):
         placeholder_output: str = None,
         exec_fn: Callable = None,
     ):
+        # Initialize a Command object to store command and its details of a certain framework.
         self.function_name = function_name
         self.args = args
         self.kwargs = kwargs
@@ -27,14 +54,24 @@ class Command(object):
         self.frameworks = {"tf": "tensorflow", "torch": "torch"}
 
     def execute_routine(self, store={}):
+        """
+        Executes a translated command
+
+        :param store: Stores key value arguments of a command.
+        :type store: dict
+        """
         if self.exec_fn is None:
+            # Copy full API Path of command
             attrs = self.attrs.copy()
+            # Get module of converted framework
             translated_cmd = sys.modules[self.frameworks[attrs.pop(0)]]
+            # Extract the command name from the full API Path
             while len(attrs) > 0:
                 translated_cmd = getattr(translated_cmd, attrs.pop(0))
             self.exec_fn = translated_cmd
 
         args = []
+        # Map command arguments to appropriate key value pairs
         for arg in self.args:
             if isinstance(arg, Placeholder):
                 args.append(store[arg.key])
@@ -44,11 +81,23 @@ class Command(object):
 
 
 def cmd_from_info(info, store: dict) -> Command:
+    """
+    Creates a command given info and arguments with values.
+
+    :param info: Details of attributes of a command
+    :type info: dict
+    :param store: Stores key value arguments of a command.
+    :type store: dict
+
+    :return: Returns a Command object populated with args & kwargs
+    :rtype:  Command
+    """
     args = []
     kwargs = {}
     for arg in info["args"]:
         is_kwarg = arg["kwarg"]
         name = arg.get("placeholder_input", arg["name"])
+        # Map args and kwargs from input to command argmuents
         if is_kwarg is True:
             if name in store:
                 kwargs[name] = store[name]
@@ -56,6 +105,7 @@ def cmd_from_info(info, store: dict) -> Command:
             if name in store:
                 args.append(store[name])
             else:
+                # Placeholder for normal args
                 args.append(Placeholder(name))
 
     return Command(
--- pythreepio/pythreepio/errors.py
+++ pythreepio/pythreepio/errors.py
@@ -1,10 +1,7 @@
 class TranslationMissing(Exception):
+    # Exception for commands which currently do not support translation.
+
     def __init__(self, name):
         super().__init__(
             f"Translation for the command {name} is not currently supported"
         )
-
-
-class NotTranslated(Exception):
-    def __init__(self):
-        super().__init__("Translation must be completed before executing")
